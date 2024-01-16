01f83e282973164c33e177b918d1d68b852c8341
Entreprenerd <alex@entreprenerd.xyz>
2024-01-16 13:39:23 +0100

    fix: import `FoundryAsserts`

--- src/BaseCryticToFoundry.t.sol
+++ src/BaseCryticToFoundry.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import {Test} from "forge-std/Test.sol";
 import {BaseTargetFunctions} from "./BaseTargetFunctions.sol";
+import {FoundryAsserts} from "./FoundryAsserts.sol";
 
 abstract contract BaseCryticToFoundry is Test, BaseTargetFunctions, FoundryAsserts {
     function setUp() public {
